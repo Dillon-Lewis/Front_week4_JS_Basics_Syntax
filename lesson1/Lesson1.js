2daa9cc1621b705ff9d77ab3510a4005c8215ce7
Dillon-Lewis <dilllewis42@gmail.com>
2024-07-30 13:28:05 -0400

    First commit

---
+++ lesson1/Lesson1.js
@@ -0,0 +1,11 @@
+const person = "Dillon";
+let age = 30;
+let grades = {math: 95, science: 92, english: 85};
+let isGoodAttendence = true;
+
+
+console.log("Name:", person);
+console.log("Age:", age);
+console.log("Grades", grades);
+console.log("In good attendence standings", isGoodAttendence);
+
---
+++ lesson2/Lesson2.js
@@ -0,0 +1,31 @@
+let x = 12;
+let y = 4;
+
+let sum = x + y;
+let sub = x - y;
+let product = x * y;
+let quotient = x / y;
+
+console.log("Sum:", sum);
+console.log("Difference:", sub);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+let NewNum = 12;
+
+let Updatedsum = sum + NewNum;
+let Updatedsub = sub - NewNum;
+let Updatedproduct = product * NewNum;
+let Updatedquotient = quotient / NewNum;
+
+console.log("Updated Sum:", Updatedsum);
+console.log("Updated Sum:", Updatedsub);
+console.log("Updated Sum:", Updatedproduct);
+console.log("Updated Sum:", Updatedquotient);
+
+
+let isEqualto = Updatedproduct == sum
+
+console.log("Is your updated product equal to your original sum of numbers:", isEqualto);
+
+
